1e23cce6720b9458d98171a456d546b5715fe3c8
Yannick Gloster <yannickgloster@gmail.com>
2022-12-22 13:11:03 -0800

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
