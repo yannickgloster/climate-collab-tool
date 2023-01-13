15986a9e224f643cd0d4f3f073588bfa40da9ddb
Yannick Gloster <yannickgloster@gmail.com>
2023-01-12 18:23:58 -0800

    Fixed tests to match new game format

--- cypress/component/Game.cy.ts
+++ cypress/component/Game.cy.ts
@@ -7,7 +7,7 @@ describe("Game Class", () => {
       userId: "34203a59-39dd-4d81-bf17-06ed793d6804",
       gameCode: gameCode,
     };
-    const game = new Game(user, gameCode);
+    const game = new Game(gameCode, [user]);
 
     expect(game.gameCode).to.equal(gameCode);
     expect(game.users.length).to.equal(1);
@@ -24,7 +24,7 @@ describe("Game Class", () => {
       userId: "fd946d2c-01b6-425f-839e-b666c12d5255",
       gameCode: gameCode,
     };
-    const game = new Game(user, gameCode);
+    const game = new Game(gameCode, [user]);
 
     game.addUser(user2);
     expect(game.users.length).to.equal(2);
@@ -42,7 +42,7 @@ describe("Game Class", () => {
       userId: "fd946d2c-01b6-425f-839e-b666c12d5255",
       gameCode: gameCode,
     };
-    const game = new Game(user, gameCode);
+    const game = new Game(gameCode, [user]);
 
     game.addUser(user2);
     game.removeUser(user2);
@@ -61,7 +61,7 @@ describe("Game Class", () => {
       userId: "fd946d2c-01b6-425f-839e-b666c12d5255",
       gameCode: gameCode,
     };
-    const game = new Game(user, gameCode);
+    const game = new Game(gameCode, [user]);
 
     let timestamp = game.timestamp;
     cy.wait(1);
