65af43c52c4d296fea82470fc07325ad032b4a85
Yannick Gloster <yannickgloster@gmail.com>
2023-01-26 17:48:32 +0000

    added some strucutre to python

---
+++ data-preprocessing/constants.py
@@ -0,0 +1,17 @@
+# Points which surround each region.
+# TODO: use this: https://is-enes-data.github.io/cordex_archive_specifications.pdf
+# TODO: Look into : https://sandbox.zenodo.org/record/870510
+regions = {
+    "EU": {"full_name": "European Union", "points": [[]]},
+    "US": {"full_name": "United States", "points": [[]]},
+    "China": {"full_name": "China", "points": [[]]},
+    "SA": {
+        "full_name": "South American Amazonian Countries",
+        "points": [[]],
+    },  # Brazil, Columbia, Bolivia, Peru, Ecuador, Venezuela, Suriname, Guyana)
+    "India": {"full_name": "India", "points": [[]]},
+    "Island": {
+        "full_name": "Alliance of Small Island States",
+        "points": [[]],
+    },  # https://en.wikipedia.org/wiki/Alliance_of_Small_Island_States
+}
--- data-preprocessing/main.py
+++ data-preprocessing/main.py
@@ -4,8 +4,44 @@ import xarray as xr
 import matplotlib.path as mpath
 import matplotlib.pyplot as plt
 
-ds = xr.open_dataset(
-    f"data/ta_Amon_EC-Earth3-Veg_ssp119_r1i1p1f1_gr_20150116-20991216_v20200226.nc"
+from shapely.geometry import Point, Polygon
+
+datasets_root = "data"
+
+monthly_air_temperature_2015_to_2099_netCDF = (
+    "ta_Amon_EC-Earth3-Veg_ssp119_r1i1p1f1_gr_20150116-20991216_v20200226.nc"
 )
 
-print(ds)
+ds = xr.open_dataset(f"{datasets_root}/{monthly_air_temperature_2015_to_2099_netCDF}")
+
+print(ds.stack(lat_lon=["lat", "lon"]))
+
+# TODO: Average the mean for all the areas within the boundary of the 8 regions.
+
+# loop over each long/lat pairing
+
+# i = 0
+# # FIXME: this doesn't do the comment above
+# for data_point in ds.ta.groupby("lat", "lon"):
+#     print(data_point)
+#     print("-----------------------------------")
+#     i += 1
+#     if i == 2:
+#         break
+
+
+# check if it's within the area defined by the region
+"""
+Example Code:
+
+coords = [(10, 20), (30, 20), (30, 40), (10, 40), (10, 20)]
+point = Point(25, 35)
+bounding_area = Polygon(coords)
+print(point.within(bounding_area))
+"""
+
+# if it's in the region, add it to a rolling average (or sum and increase count)
+
+# once completed, store in .nc file for the time being.
+
+# TODO: Upload to DB
