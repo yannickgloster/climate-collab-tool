23cabed5fbb66274612eed440286f228dce3b74d
Yannick Gloster <yannickgloster@gmail.com>
2023-02-07 10:15:32 +0000

    Updated sources to use MUI

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -2,8 +2,6 @@ import Layout from "../components/layout";
 import Typography from "@mui/material/Typography";
 import { default as NextLink } from "next/link";
 import Link from "@mui/material/Link";
-import Image from "next/image";
-import Image404 from "../public/images/emile-seguin-R9OueKOtGGU-unsplash.jpg";
 
 export default function Custom404() {
   return (
--- app/pages/sources.tsx
+++ app/pages/sources.tsx
@@ -1,29 +1,43 @@
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { default as NextLink } from "next/link";
+
 import Layout from "../components/layout";
 
 export default function () {
   return (
     <Layout img="/images/eberhard-grossgasteiger-jCL98LGaeoE-unsplash.jpg">
-      <p>IMAGES</p>
-      <p>
+      <Typography variant="h3" textAlign="center" fontWeight={800}>
+        Sources
+      </Typography>
+      <Typography variant="body2" textAlign="center">
+        <Link component={NextLink} href="/">
+          Return Home
+        </Link>
+      </Typography>
+      <Typography variant="h4" textAlign="center" fontWeight={800}>
+        Images
+      </Typography>
+      <Typography variant="body1" textAlign="center">
         Photo by{" "}
-        <a href="https://unsplash.com/@katekerdi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        <Link href="https://unsplash.com/@katekerdi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           Katerina Kerdi
-        </a>{" "}
+        </Link>{" "}
         on{" "}
-        <a href="https://unsplash.com/photos/-YiJvbfNDqk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        <Link href="https://unsplash.com/photos/-YiJvbfNDqk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           Unsplash
-        </a>
-      </p>
-      <p>
+        </Link>
+      </Typography>
+      <Typography variant="body1" textAlign="center">
         Photo by{" "}
-        <a href="https://unsplash.com/@eberhardgross?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        <Link href="https://unsplash.com/@eberhardgross?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           eberhard 🖐 grossgasteiger
-        </a>{" "}
+        </Link>{" "}
         on{" "}
-        <a href="https://unsplash.com/images/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        <Link href="https://unsplash.com/images/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           Unsplash
-        </a>
-      </p>
+        </Link>
+      </Typography>
     </Layout>
   );
 }
