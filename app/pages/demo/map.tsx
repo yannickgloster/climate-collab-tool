6e9c320743c9c05cd2f57c03de683f8fad037a78
Yannick Gloster <yannickgloster@gmail.com>
2023-02-23 17:26:10 +0000

    added woorking map

--- app/pages/demo/map.tsx
+++ app/pages/demo/map.tsx
@@ -1,10 +1,13 @@
 import Layout from "../../components/layout";
 import Map from "../../components/map";
+import Box from "@mui/material/Box";
 
 export default function LoadingTest() {
   return (
     <Layout>
-      <Map />
+      <Box width={500} border="1px dashed grey">
+        <Map />
+      </Box>
     </Layout>
   );
 }
